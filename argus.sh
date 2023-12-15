736bdacc8f75620a063c238135c49a7f65077da5
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-14 18:45:46 -0600

    variables

---
+++ argus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+$person1= $1
+$person2=$2
+echo "$1:hii how are you"
+echo "$2:hii i am good how are you"
+echo "$1: Fine"
--- variables.sh
+++ variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "rakesh:hii how are you"
-echo "varshith:hii i am good how are you"
-echo "rakesh: Fine"
+$person1="rakesh"
+$person2="varshith"
+echo "$person1:hii how are you"
+echo "$person2:hii i am good how are you"
+echo "$person1: Fine"
