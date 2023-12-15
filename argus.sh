cf6188210c3d1f56d6da0bc877084444e24cf219
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-14 18:58:09 -0600

    argus

--- argus.sh
+++ argus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$person1= $1
-$person2=$2
+person1= $1
+person2=$2
 echo "$person1:hii how are you"
 echo "$person2:hii i am good how are you"
 echo "$person1: Fine"
--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$person1="rakesh"
-$person2="varshith"
+person1="rakesh"
+person2="varshith"
 echo "$person1:hii how are you"
 echo "$person2:hii i am good how are you"
 echo "$person1: Fine"
