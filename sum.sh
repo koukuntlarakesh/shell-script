c731325c4fb08a4af3c8a9f7ec3f8a2500741af5
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-16 07:23:19 -0600

    updated

--- sum.sh
+++ sum.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-NUMBER1 =$1
-NUMBER2 =$2
+NUMBER1 = $1
+NUMBER2 = $2
 
 
 SUM= $(( $NUMBER1 + $NUMBER2 ))    
 #DIFF=$(echo "$NUMBER1 - $NUMBER2")
       # using bc for the subtraction because bash doesn'
-echo "the number of arguments :$#"
+echo "the number of arguments:$#"
 echo "the values are :$@"
 echo "the file name is: $0"
