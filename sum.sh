d9f4db5bddf63ec579224730d310ebbf198f4f8c
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-16 07:20:06 -0600

    argus

--- sum.sh
+++ sum.sh
@@ -4,8 +4,8 @@ NUMBER1 =$1
 NUMBER2 =$2
 
 
-SUM=$(($NUMBER1)+($NUMBER2))    
-DIFF=$(echo "$NUMBER1 - $NUMBER2")
+SUM= $(( $NUMBER1 + $NUMBER2 ))    
+#DIFF=$(echo "$NUMBER1 - $NUMBER2")
       # using bc for the subtraction because bash doesn'
 echo "the number of arguments :$#"
 echo "the values are :$@"
