eb0d843e047e7277527c0519d38f8214e01c31e1
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-18 13:39:46 -0600

    array

--- logs.sh
+++ logs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 ID=$(id -u)
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m";   
+E="\e[0m"
 
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+echo " Script started at $TIMESTAMP" &>> $LOGFILE
 
 if  [ $ID -ne 0 ] 
 then 
@@ -16,10 +22,10 @@ fi
 validate() {
 if [ $1 -ne 0 ]
 then
-    echo "ERROR:: Installing $2 is failed"
+    echo "$R ERROR::$E Installing $2 is failed"
     exit 1
 else
-    echo "Installing $2 is SUCCESS"
+    echo "Installing $2 is $G SUCCESS $E"
 fi
 }
 
