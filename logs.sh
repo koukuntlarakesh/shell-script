e6ac0eb305b5adf152c46bf5fd5b55cc1924f824
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-18 19:10:59 -0600

    logs

--- logs.sh
+++ logs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-ID=$(id -u)
+ID=$(id -u) 
 
-TIMESTAMP=$( date +%F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 R="\e[31m"
 G="\e[32m"
 E="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "Script started at $TIMESTAMP" &>> $LOGFILE
+echo "Script started at $TIMESTAMP" #&>> $LOGFILE
 
 if  [ $ID -ne 0 ] 
 then 
