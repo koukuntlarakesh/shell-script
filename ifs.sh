1f52f550d2e1dc07d58abd5b0d5e02b019bd5f7c
koukuntlarakesh <koukuntlarakesh@gmail.com>
2024-01-17 19:04:08 -0600

    shell-script

---
+++ ifs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+
+SOURCE_DIRE=/etc/passwd
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ ! -d $SOURCE_DIRE ]
+then
+   
+    echo -e "$R Source Directory does not exit $N"
+    exit 1
+else
+   {
+    echo -e "$G Source exits $N"
+   }
+fi 
+
+FILES_TO_DELETE=$(find $SOURCE_DIRE -type f -mtime +14 -name "*.log")
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do
+echo "username:$username"
+echo "password:$password"
+done <<< $FILES_TO_DELETE
