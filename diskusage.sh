a35a84b430e4c7c605a3c7d917811fc4fb6991e8
koukuntlarakesh <koukuntlarakesh@gmail.com>
2024-01-17 19:48:43 -0600

    shell-script

---
+++ diskusage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+Message=""
+
+
+while IFS= read line
+do
+  usage=$(echo $line | awk '{print $6F}'| cut -d % -f1)
+  partition=$(echo $line | awk '{print $1F}')
+  if [ $usage -ge $DISK_THRESHOLD]
+  then 
+  Message+="High disk usage on $partition:$usage\n"
+fi
+done <<< $DISK_USAGE
+
+echo -e "Message:$Message"
